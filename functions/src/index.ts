cd4f4f58ad96c34a38b1da0320452bc268c25ff1
nabettu <tatsuaki.w@gmail.com>
2019-10-30 23:12:37 +0900

    3-1まで

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,17 @@
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
+import { collectionName } from './services/mangarel/constants';
+
+admin.initializeApp();
+
+export const publishers = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (req, res) => {
+    const snap = await admin
+      .firestore()
+      .collection(collectionName.publishers)
+      .get();
+    const data = snap.docs.map(doc => doc.data());
+    res.send({ data });
+  });
